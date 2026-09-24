74fb89a1cf7f617f46e9f41eaa63128c1e2bfe11
alibabs <au@nul.se>
2026-09-24 13:55:36 +0200

    add: piece class to core

---
+++ Luffarschack.Core/Piece.cs
@@ -0,0 +1,20 @@
+namespace Luffarschack.Core;
+
+public class Piece
+{
+    public int _x { get; set; }
+    public int _y { get; set; }
+    public int _z { get; set; }
+    
+    public Piece()
+    {
+        _x = 0;
+        _y = 0;
+        _z = 0;
+    }
+
+    public Piece(int x, int y, int z)
+    {
+        
+    }
+}

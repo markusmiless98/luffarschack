1664c8fed76818424019d2de7d891200a877e21a
alibabs <au@nul.se>
2026-09-25 14:13:41 +0200

    refactor and add value object
    add player class
    refactor GameState.cs moved into Entites dir
    refactor Boardstate to contain player instead of int
    add ApplyMove to GameState.cs
    refactor Move.cs to have Player object instead of string value
    refactor Moved Move.cs and Player.cs into ValueObjects dir

--- Luffarschack.Core/Entities/GameState.cs
+++ Luffarschack.Core/Entities/GameState.cs
@@ -2,9 +2,10 @@ namespace Luffarschack.Core;
 
 public class GameState
 {
-    public int[,,] BoardState { get; set; }
+    public Player[,,] BoardState { get; set; }
     public string[] Players { get; set; }
     public string? Winner { get; set; }
     //public string PlayerTurn { get; set; }
     public int Turn { get; set; }
+    public void ApplyMove(Move move) => BoardState[move.x, move.y, move.z] = move.Player;//try catch for out of index exception
 }
---
+++ Luffarschack.Core/ValueObjects/Player.cs
@@ -0,0 +1,8 @@
+namespace Luffarschack.Core;
+
+public class Player
+{
+    public int PlayerId { get; set; }
+    public string Name { get; set; }
+    public string Color { get; set; }
+}

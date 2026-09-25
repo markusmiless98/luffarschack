df91efec74a29f227418a1bc785f8e23c630c8a3
alibabs <au@nul.se>
2026-09-25 14:35:40 +0200

    add and refactor Board
    add Board.cs object, refactor Gamestate.cs Boardstate type, refactor moved Player to Entities and GameState.cs to ValueObjects

--- Luffarschack.Core/ValueObjects/GameState.cs
+++ Luffarschack.Core/ValueObjects/GameState.cs
@@ -2,10 +2,10 @@ namespace Luffarschack.Core;
 
 public class GameState
 {
-    public Player[,,] BoardState { get; set; }
+    public Board BoardState { get; set; }
     public string[] Players { get; set; }
     public string? Winner { get; set; }
     //public string PlayerTurn { get; set; }
     public int Turn { get; set; }
-    public void ApplyMove(Move move) => BoardState[move.x, move.y, move.z] = move.Player;//try catch for out of index exception
+    public void ApplyMove(Move move) => BoardState.Positions[move.x, move.y, move.z] = move.Player;//check IsWithinBounds with if statement
 }

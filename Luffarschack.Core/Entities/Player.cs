a6802321fda26f92e52e10097b2a5cf6d2bae9ec
alibabs <au@nul.se>
2026-09-25 14:48:29 +0200

    change player id prop

--- Luffarschack.Core/Entities/Player.cs
+++ Luffarschack.Core/Entities/Player.cs
@@ -2,7 +2,7 @@ namespace Luffarschack.Core;
 
 public class Player
 {
-    public int PlayerId { get; set; }
+    public int Id { get; set; }
     public string Name { get; set; }
     public string Color { get; set; }
 }
